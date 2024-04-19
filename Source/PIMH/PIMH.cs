35431e7bea948fb1963eb9ed11201a857622778f
flestal <y2s0605@gmail.com>
2024-04-19 18:55:19 +0900

    Initial commit

---
+++ Source/PIMH/PIMH.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Verse;
+using HarmonyLib;
+using System.Reflection;
+
+namespace PIMH
+{
+    [StaticConstructorOnStartup]
+    public static class PIMH
+    {
+        static PIMH()
+        {
+            new Harmony("Flestal.PIMH").PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ Source/PIMH/TryGiveJob_Patch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace PIMH
+{
+    [HarmonyPatch(typeof(Pawn_InfectionVectorTracker), "AddInfectionVector",new Type[] {typeof(InfectionPathway)})]
+    public static class AddInfectionVector_Patch
+    {
+        public static bool Prefix(InfectionPathway pathway)
+        {
+            bool Immune = pathway.OwnerPawn.story.traits.HasTrait(TraitDefOf.BodyPurist);
+            if (Immune)
+            {
+                Log.Message(pathway.OwnerPawn + " is immune to metalhorror.");
+                return false;
+            }
+            return true;
+        }
+    }
+}
